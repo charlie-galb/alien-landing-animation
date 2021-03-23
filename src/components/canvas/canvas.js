95b19a8b54375ae696e3946a2a4a8662b9ba183b
charlie-galb <galbraith.charlie@gmail.com>
2021-03-23 16:54:47 +0000

    Add flex display to internal contianer div

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { UFO } from '../UFO/UFO'
+import { Background } from '../background/background'
 
 export const Canvas = () => {
 
     return (
-        <div style={{display: 'flex', justifyContent: 'center', paddingTop: '100px', border: '5px solid red', width: '800px', height: '450px'}}>
-            <UFO positionY={50}/>
+        <div style={{display: 'flex', alignContent: "center", justifyContent: 'center', paddingTop: '100px', }}>
+            <div style={{display: 'flex', alignContent: "center", justifyContent: 'center',  border: '5px solid red', width: '800px', height: '450px'}}>
+            <Background />
+                <UFO />
+              
+            </div>
         </div>
     )
 }
