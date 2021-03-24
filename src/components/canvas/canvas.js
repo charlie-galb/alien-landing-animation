291189f5e517395ae578e08a9005a0b1cb6350be
charlie-galb <galbraith.charlie@gmail.com>
2021-03-24 13:46:37 +0000

    Delete erroneous comma

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -5,7 +5,7 @@ import { Background } from '../background/background'
 export const Canvas = () => {
 
     return (
-        <div style={{display: 'flex', alignContent: "center", justifyContent: 'center', paddingTop: '100px', }}>
+        <div style={{display: 'flex', alignContent: "center", justifyContent: 'center', paddingTop: '100px'}}>
             <svg style={{width: '800px', height: '450px', border: '5px solid red'}}>
                 <Background />
                 <UFO />
