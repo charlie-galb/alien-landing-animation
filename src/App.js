636ca04c797728023447d791e3a21faa6b1398cd
charlie-galb <galbraith.charlie@gmail.com>
2021-03-24 13:47:00 +0000

    Add selective rendering of play button and animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React , {useState} from 'react'
+
 import { Canvas } from './components/canvas/canvas';
 
 function App() {
+  const [isNotYetPlayed, setIsNotYetPlayed] = useState(true)
+  const [isPlaying, setisPlaying] = useState(false)
+
+  const handleClick = () => {
+    setIsNotYetPlayed(false)
+    setisPlaying(true)
+  }
+
+  const renderPlayButton = () => {
+    if (isNotYetPlayed) {
+      return (
+      <button style={{marginTop: "35%", marginLeft: "Auto", marginRight: "auto", width: "100px", height: "60px", fontSize: "20px"}} onClick={handleClick}>Play</button>
+      )
+    }
+  }
+
+  const renderAnimation = () => {
+    if (isPlaying) {
+      return (
+        <div>
+          <Canvas></Canvas>
+        </div>
+      )
+    }
+  }
+
+
   return (
-    <div>
-      <Canvas></Canvas>
+    <div style={{display: 'flex', flexDirection: "column", alignContent: "center", justifyContent: 'center'}}>
+      {renderAnimation()}
+      {renderPlayButton()}
     </div>
   );
 }
