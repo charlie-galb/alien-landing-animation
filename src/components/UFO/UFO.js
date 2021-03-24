9e2f808e4affd6c0f48f11ac0fe4d32498bb8f29
charlie-galb <galbraith.charlie@gmail.com>
2021-03-24 13:45:43 +0000

    correct formatting of clipPath attribute in circle element

--- src/components/UFO/UFO.js
+++ src/components/UFO/UFO.js
@@ -33,7 +33,7 @@ export const UFO = (props) => {
                 </clipPath>
             </defs>
             <circle cx="50%" cy="50%" r="75" fill="grey" />
-            <circle cx="50%" cy="50%" r="75" stroke="grey" fill="pink" clip-path="url(#cut-off)"/>
+            <circle cx="50%" cy="50%" r="75" stroke="grey" fill="pink" clipPath="url(#cut-off)"/>
             <ellipse cx="50%" cy="50%" rx="150" ry="30" stroke="grey" fill="grey" strokeWidth="5"/>
             <ellipse cx="50%" cy="32%" rx="68" ry="15" fill="pink" strokeWidth="5"/>
             <path className={styles.halfwayLine} d="M 5 75 Q 140 110 306 75" stroke="black" strokeWidth="5" fill="transparent"/>
