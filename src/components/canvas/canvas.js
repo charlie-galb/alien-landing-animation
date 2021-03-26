b8a7994d171f118cedda097f8c4149f8432cbe21
charlie-galb <galbraith.charlie@gmail.com>
2021-03-26 14:48:51 +0000

    Move canvas styling into css module

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,12 +1,14 @@
 import React from 'react'
+
 import { UFO } from '../UFO/UFO'
 import { Background } from '../background/background'
+import styles from './canvas.module.css'
 
 export const Canvas = () => {
 
     return (
-        <div style={{display: 'flex', alignContent: "center", justifyContent: 'center', paddingTop: '100px'}}>
-            <svg style={{width: '800px', height: '450px', border: '5px solid red'}}>
+        <div className={styles.canvasContainer}>
+            <svg className={styles.canvas}>
                 <Background />
                 <UFO />
             </svg>
